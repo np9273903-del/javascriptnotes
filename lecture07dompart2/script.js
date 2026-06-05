d068888a0b7e53133c85108823e068f7d9d36e74
apnacollege-demo <np9273903@gmail.com>
2026-06-05 15:31:57 +0530

    addition of dom part 2

---
+++ lecture07dompart2/script.js
@@ -0,0 +1,46 @@
+//getAttribute
+
+
+// let para = document.querySelector("p");
+
+// let classValue = para.getAttribute("class");
+
+// console.log(classValue);
+
+
+// //changing cs using javascript
+
+// //div.style.visibility="hidden";
+// let change=document.querySelector("div");
+// change.style.backgroundColor="blue";
+
+// change.style.fontSize="26px";
+
+
+// //--------insert elements in dom ---------
+
+// let newbtn=document.createElement("button");
+// newbtn.innerText="click me!";
+// console.log(newbtn);
+
+// let div=document.querySelector("div");
+// div.prepend(newbtn);
+
+// //adding new heading using js
+
+// let newheading=document.createElement("h1");
+// newheading.innerHTML="<i>hi,i am new!</i>";
+// document.querySelector("body").prepend(newheading);
+
+// let para=document.querySelector("p");
+// para.remove();
+
+
+//------js question-----
+
+
+//1.create a button
+
+let newbutton=document.createElement("button");
+newbutton.innerText="click karo";
+console.log(newbutton);
