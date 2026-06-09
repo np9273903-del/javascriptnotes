99f9a067473be554dac7ce0dc7e7b44c21e28e87
apnacollege-demo <np9273903@gmail.com>
2026-06-09 16:10:02 +0530

    made a project tictactoc

--- tictacGame08/app.js
+++ tictacGame08/app.js
@@ -1,6 +1,8 @@
 let boxes=document.querySelectorAll(".box");
 let reset=document.querySelector("#reset");
-
+let newgamebtn=document.querySelector("#new-btn");
+let msgcontainer=document.querySelector(".msg-container");
+let msg=document.querySelector("#msg");
 let turnO=true;
 
 //2d array
@@ -17,8 +19,20 @@ const winpattern=[
     [2,4,6]
 ];
 
-//boxes.forEach(()=>{});
 
+
+const resetgame=()=>{
+    let turnO=true;
+    enableBoxes();
+    msgcontainer.classList.add("hide");
+}
+
+
+
+
+
+//boxes.forEach(()=>{});
+//boxes.forEach(()=>{});
 boxes.forEach((box)=>{
     box.addEventListener("click",()=>{
         console.log("box was clicket");
@@ -31,5 +45,46 @@ boxes.forEach((box)=>{
             turnO=true;
         }
         box.disabled=true;
+        checkWinner();
     });
 });
+
+const enableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+}
+
+const disableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=true;
+    }
+}
+
+const showWinner=(winner)=>{
+    msg.innerText=`congratulation Winner is ${winner}`;
+    msgcontainer.classList.remove("hide");
+    disableBoxes();
+
+}
+
+const checkWinner=()=>{
+    for(let pattern of winpattern){
+    let pos1val=boxes[pattern[0]].innerText;
+    let pos2val=boxes[pattern[1]].innerText;
+    let pos3val=boxes[pattern[2]].innerText;
+    
+    if(pos1val !=""&& pos2val!=""&& pos3val!="")
+    {
+        if(pos1val===pos2val && pos2val===pos3val){
+            console.log("winner",pos1val);
+            
+            showWinner(pos1val);
+        }
+    }
+    }
+}
+
+newgamebtn.addEventListener("click",resetgame);
+reset.addEventListener("click",resetgame);
